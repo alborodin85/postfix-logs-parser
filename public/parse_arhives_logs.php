8482989487e510be1aeda83b0a14ddb64cbd8768
Alexandr Borodin <alborodin85@mail.ru>
2023-05-12 18:18:57 +0300

    Файлы запуска

--- public/parse_arhives_logs.php
+++ public/parse_arhives_logs.php
@@ -6,3 +6,49 @@ $dotenv = Dotenv\Dotenv::createImmutable('../');
 $dotenv->load();
 
 $logFolderPath = $_ENV['LOG_FOLDER_PATH'];
+
+$serviceRestApiRequest = new \App\RestApi\ServiceRestApiRequest(
+    $_ENV['WEB_PATH_SCHEMA'], $_ENV['WEB_PATH_DOMAIN'], $_ENV['ENDPOINT_API_TOKEN']
+);
+$getLastArchiveResponse = $serviceRestApiRequest->post($_ENV['ENDPOINT_GET_LAST_ARCHIVE'], []);
+
+if ($getLastArchiveResponse->errorCode) {
+    if (!$_ENV['IS_TESTING']) {
+        die('не удалось получить название последнего архива');
+    }
+}
+
+$payloadGetLastArchive = new \App\RestApi\EntityResponseGetLastArchive($getLastArchiveResponse);
+$lastArchiveFileName = $payloadGetLastArchive->lastArchiveFileName;
+if ($_ENV['IS_TESTING']) {
+    $lastArchiveFileName = '';
+}
+
+$allArchives = glob($_ENV['ARCHIVE_LOG_PATTERN']);
+
+$rowParser = new \App\ParserRows();
+$composerByQueues = new \App\ComposerByQueues();
+$parserQueuePayload = new \App\ParserQueuePayload();
+
+$allMailsArray = [];
+foreach ($allArchives as $archiveName) {
+    if ($archiveName <= $lastArchiveFileName) {
+        continue;
+    }
+    $sourceLinesArray = gzfile($archiveName);
+    $logRows = $rowParser->parseArray($sourceLinesArray);
+    $queuesArray = $composerByQueues->buildQueues($logRows);
+    $mailsArray = $parserQueuePayload->parseQueuesArray($queuesArray);
+    $allMailsArray = array_merge($allMailsArray, $mailsArray);
+}
+
+$chunkedMailsArray = array_chunk($allMailsArray, (int)$_ENV['ADD_RECORDS_CHUNK_SIZE']);
+foreach ($chunkedMailsArray as $chunk) {
+    $entityRequestAddRecords = new \App\RestApi\EntityRequestAddRecords($chunk);
+    $serviceRestApiRequest->post($_ENV['ENDPOINT_ADD_ARCHIVE_RECORDS'], $entityRequestAddRecords->toArFields());
+}
+
+var_export($allMailsArray);
+echo "\n";
+
+echo 'end';
--- public/parse_current_log.php
+++ public/parse_current_log.php
@@ -8,28 +8,23 @@ $dotenv->load();
 $currentLogPath = $_ENV['CURRENT_LOG_FILE'];
 
 $rowParser = new \App\ParserRows();
-
-$logFile = fopen($currentLogPath, 'r+t');
-$logRows = [];
-while (!feof($logFile)) {
-    $sourceLine = fgets($logFile);
-    $sourceLine = trim($sourceLine);
-    if (!$sourceLine) {
-        continue;
-    }
-    $logRow = $rowParser->parse($sourceLine);
-    $logRows[] = $logRow;
-}
+$logRows = $rowParser->parseFile($currentLogPath);
 
 $composerByQueues = new \App\ComposerByQueues();
 $queuesArray = $composerByQueues->buildQueues($logRows);
 
 $parserQueuePayload = new \App\ParserQueuePayload();
-$mailsArray = [];
+$mailsArray = $parserQueuePayload->parseQueuesArray($queuesArray);
 
-foreach ($queuesArray as $queueItem) {
-    $mails = $parserQueuePayload->buildMailMessage($queueItem);
-    $mailsArray = array_merge($mailsArray, $mails);
+$serviceRestApiRequest = new \App\RestApi\ServiceRestApiRequest(
+    $_ENV['WEB_PATH_SCHEMA'], $_ENV['WEB_PATH_DOMAIN'], $_ENV['ENDPOINT_API_TOKEN']
+);
+$serviceRestApiRequest->post($_ENV['ENDPOINT_CLEAR_CURRENT_RECORDS'], []);
+
+$chunkedMailsArray = array_chunk($mailsArray, (int)$_ENV['ADD_RECORDS_CHUNK_SIZE']);
+foreach ($chunkedMailsArray as $chunk) {
+    $entityRequestAddRecords = new \App\RestApi\EntityRequestAddRecords($chunk);
+    $serviceRestApiRequest->post($_ENV['ENDPOINT_ADD_CURRENT_RECORDS'], $entityRequestAddRecords->toArFields());
 }
 
 var_export($mailsArray);
