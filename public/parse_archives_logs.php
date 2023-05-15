409d11d11635396dd15768351574ba11ce042ffc
ady <ady@infoservice.ru>
2023-05-16 00:28:24 +0300

    Правки с прода

--- public/parse_archives_logs.php
+++ public/parse_archives_logs.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable('../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 $logFolderPath = $_ENV['LOG_FOLDER_PATH'];
--- public/parse_current_log.php
+++ public/parse_current_log.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable('../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 $currentLogPath = $_ENV['CURRENT_LOG_FILE'];
